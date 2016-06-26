30d40ae52feaf25286f7eea3a75ca85659b2df2a
ragu <raphael.guye@gmail.com>
2016-06-26 20:50:01 +0200

    Replace the MainTabbedPage by a XAML class

---
+++ GoldenBook/GoldenBook/Views/MainTabbedPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace GoldenBook.Views
+{
+    public partial class MainTabbedPage : TabbedPage
+    {
+        public MainTabbedPage()
+        {
+            InitializeComponent();
+
+            Children.Add(new AdvertisersFormPage());
+            Children.Add(new AdvertisersPage());
+        }
+    }
+}
