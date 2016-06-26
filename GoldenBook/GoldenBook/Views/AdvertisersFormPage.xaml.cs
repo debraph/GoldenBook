3736802ed993b60fa9957a707c6a129a97f054fe
ragu <raphael.guye@gmail.com>
2016-06-26 20:41:10 +0200

    Replace the AdvertisersFormPage by a XAML class

---
+++ GoldenBook/GoldenBook/Views/AdvertisersFormPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace GoldenBook.Views
+{
+    public partial class AdvertisersFormPage : ContentPage
+    {
+        public AdvertisersFormPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnSendButton_Clicked(object sender, EventArgs args)
+        {
+            // TODO
+        }
+    }
+}
