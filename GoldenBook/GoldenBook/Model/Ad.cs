12205339472fde74dbb3e466f94629358313c03b
ragu <raphael.guye@gmail.com>
2016-08-07 17:42:40 +0200

    Add NuGet package Microsoft.Azure.Mobile.Client and use the Azure API to save the entry in the cloud

---
+++ GoldenBook/GoldenBook/Model/Ad.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace GoldenBook.Model
+{
+    public class Ad
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("createdAt")]
+        public DateTime CreatedAt { get; set; }
+
+        [JsonProperty("updatedAt")]
+        public DateTime UpdatedAt { get; set; }
+
+        [JsonProperty("deleted")]
+        public bool IsDeleted { get; set; }
+
+        [JsonProperty("message")]
+        public string Message { get; set; }
+
+        [JsonProperty("firstName")]
+        public string FirstName { get; set; }
+
+        [JsonProperty("lastName")]
+        public string LastName { get; set; }
+
+        [JsonProperty("email")]
+        public string Email { get; set; }
+
+        [JsonProperty("addedBy")]
+        public string AddedBy { get; set; }
+
+        [JsonProperty("amount")]
+        public float Amount { get; set; }
+    }
+}
