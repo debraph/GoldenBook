9b6673efa2f36e7928d67b8e8b0cc29cdf168db6
ragu <raphael.guye@gmail.com>
2016-09-10 12:59:37 +0200

    Implement IOSMediaService.SavePictureAndThumbnail

--- GoldenBook/GoldenBook.iOS/Services/IOSMediaService.cs
+++ GoldenBook/GoldenBook.iOS/Services/IOSMediaService.cs
@@ -1,10 +1,40 @@
 ﻿using GoldenBook.ServiceContract;
 using System;
+using System.IO;
 
 namespace GoldenBook.iOS.Services
 {
     public class IOSMediaService : IMediaService
     {
+        private string AppDirectoryPath => Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "GoldenBook");
+        private string FilePath(string filename) => Path.Combine(AppDirectoryPath, filename);
+
+        public string SavePictureAndThumbnail(byte[] picture, string filename)
+        {
+            try
+            {
+                var thumbnails = CreateThumbnails(picture);
+
+                if (!Directory.Exists(AppDirectoryPath))
+                {
+                    var info = Directory.CreateDirectory(AppDirectoryPath);
+                    if (!info.Exists) return null;
+                }
+
+                var filePath = FilePath(filename);
+                var thumbnailPath = $"{filePath}_thumb";
+
+                if (!WriteFile(picture, filePath)) return null;
+                if (!WriteFile(thumbnails, thumbnailPath)) return null;
+
+                return filePath;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
         public string GetFilepath(string pictureId)
         {
             return null;
@@ -15,9 +45,29 @@ namespace GoldenBook.iOS.Services
             return null;
         }
 
-        public string SavePictureAndThumbnail(byte[] picture, string filename)
+        #region Private methods
+
+        private bool WriteFile(byte[] picture, string filePath)
         {
-            return null;
+            try
+            {
+                if (File.Exists(filePath)) return true;
+
+                File.WriteAllBytes(filePath, picture);
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
         }
+
+        private byte[] CreateThumbnails(byte[] picture)
+        {
+            return picture;
+        }
+
+        #endregion
     }
 }
