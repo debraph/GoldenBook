96f0fa4ac73e2f28b976e961c381d77d2a00d8ed
ragu <raphael.guye@gmail.com>
2016-09-10 12:20:47 +0200

    Implement classes AppDelegate and IOSMediaService

---
+++ GoldenBook/GoldenBook.iOS/Services/IOSMediaService.cs
@@ -0,0 +1,23 @@
+﻿using GoldenBook.ServiceContract;
+using System;
+
+namespace GoldenBook.iOS.Services
+{
+    public class IOSMediaService : IMediaService
+    {
+        public string GetFilepath(string pictureId)
+        {
+            return null;
+        }
+
+        public Tuple<string, byte[]> ProcessCapturedPhoto(string filePath, bool needXMirroring = false)
+        {
+            return null;
+        }
+
+        public string SavePictureAndThumbnail(byte[] picture, string filename)
+        {
+            return null;
+        }
+    }
+}
