65e33333325347b4d83d6bfcd1ec389c111115d5
ragu <raphael.guye@gmail.com>
2016-09-19 18:51:56 +0200

    Add a NumberValidaorBehavior for the amount of the Ad

---
+++ GoldenBook/GoldenBook/Behaviors/NumberValidatorBehavior.cs
@@ -0,0 +1,35 @@
+﻿using Xamarin.Forms;
+
+namespace GoldenBook.Behaviors
+{
+    public class NumberValidatorBehavior : Behavior<Entry>
+    {
+        static readonly BindablePropertyKey IsValidPropertyKey = BindableProperty.CreateReadOnly("IsValid", typeof(bool), typeof(NumberValidatorBehavior), false);
+        public static readonly BindableProperty IsValidProperty = IsValidPropertyKey.BindableProperty;
+        public bool IsValid
+        {
+            get { return (bool)base.GetValue(IsValidProperty); }
+            private set { base.SetValue(IsValidPropertyKey, value); }
+        }
+
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            bindable.TextChanged += HandleTextChanged;
+        }
+
+        void HandleTextChanged(object sender, TextChangedEventArgs e)
+        {
+            int value;
+            bool isParsingSuccess = int.TryParse(e.NewTextValue, out value);
+
+            IsValid = isParsingSuccess && value > 0;
+
+            ((Entry)sender).TextColor = IsValid ? Color.Default : Color.Red;
+        }
+
+        protected override void OnDetachingFrom(Entry bindable)
+        {
+            bindable.TextChanged -= HandleTextChanged;
+        }
+    }
+}
