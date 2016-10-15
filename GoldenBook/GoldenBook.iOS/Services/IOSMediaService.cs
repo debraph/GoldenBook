f350dca0fb5d3da90c55ccda801ebdbcc33447aa
Raphael Guye <raphael.guye@swatchgroup.com>
2016-10-15 16:02:43 +0200

    Fix the rotation of the photo captured on iOS

--- GoldenBook/GoldenBook.iOS/Services/IOSMediaService.cs
+++ GoldenBook/GoldenBook.iOS/Services/IOSMediaService.cs
@@ -4,6 +4,7 @@ using GoldenBook.ServiceContract;
 using System;
 using System.IO;
 using UIKit;
+using System.Drawing;
 
 namespace GoldenBook.iOS.Services
 {
@@ -47,7 +48,26 @@ namespace GoldenBook.iOS.Services
         public Tuple<string, byte[]> ProcessCapturedPhoto(string filePath, bool needXMirroring = false)
         {
             UIImage image = UIImage.FromFile(filePath);
-            var imageByteArray = UIImageToByteArray(image);
+
+			var orientation = image.Orientation;
+			UIImage imageTransformed;
+			switch (orientation)
+			{
+				case UIImageOrientation.Right:
+					imageTransformed = ScaleAndRotateImage(image, UIImageOrientation.Right);
+					break;
+				case UIImageOrientation.Down:
+					imageTransformed = ScaleAndRotateImage(image, UIImageOrientation.Down);
+					break;
+				case UIImageOrientation.Left:
+					imageTransformed = ScaleAndRotateImage(image, UIImageOrientation.Left);
+					break;
+				default:
+					imageTransformed = image;
+					break;
+			}
+
+            var imageByteArray = UIImageToByteArray(imageTransformed);
 
             var id = Guid.NewGuid().ToString("n");
             var filename = $"capture-{id}";
@@ -58,6 +78,97 @@ namespace GoldenBook.iOS.Services
         }
 
         #region Private methods
+
+		UIImage ScaleAndRotateImage(UIImage imageIn, UIImageOrientation orIn)
+		{
+			CGImage imgRef = imageIn.CGImage;
+			float width = imgRef.Width;
+			float height = imgRef.Height;
+			CGAffineTransform transform = CGAffineTransform.MakeIdentity();
+			RectangleF bounds = new RectangleF(0, 0, width, height);
+
+			switch (orIn)
+			{
+				case UIImageOrientation.Up:                                        //EXIF = 1
+					transform = CGAffineTransform.MakeIdentity();
+					break;
+
+				case UIImageOrientation.UpMirrored:                                //EXIF = 2
+					transform = CGAffineTransform.MakeTranslation(imageSize.Width, 0f);
+					transform = CGAffineTransform.MakeScale(-1.0f, 1.0f);
+					break;
+
+				case UIImageOrientation.Down:                                      //EXIF = 3
+					transform = CGAffineTransform.MakeTranslation(imageSize.Width, imageSize.Height);
+					transform = CGAffineTransform.Rotate(transform, (float)Math.PI);
+					break;
+
+				case UIImageOrientation.DownMirrored:                              //EXIF = 4
+					transform = CGAffineTransform.MakeTranslation(0f, imageSize.Height);
+					transform = CGAffineTransform.MakeScale(1.0f, -1.0f);
+					break;
+
+				case UIImageOrientation.LeftMirrored:                              //EXIF = 5
+					boundHeight = bounds.Height;
+					bounds.Height = bounds.Width;
+					bounds.Width = boundHeight;
+					transform = CGAffineTransform.MakeTranslation(imageSize.Height, imageSize.Width);
+					transform = CGAffineTransform.MakeScale(-1.0f, 1.0f);
+					transform = CGAffineTransform.Rotate(transform, 3.0f * (float)Math.PI / 2.0f);
+					break;
+
+				case UIImageOrientation.Left:                                      //EXIF = 6
+					boundHeight = bounds.Height;
+					bounds.Height = bounds.Width;
+					bounds.Width = boundHeight;
+					transform = CGAffineTransform.MakeTranslation(0.0f, imageSize.Width);
+					transform = CGAffineTransform.Rotate(transform, 3.0f * (float)Math.PI / 2.0f);
+					break;
+
+				case UIImageOrientation.RightMirrored:                             //EXIF = 7
+					boundHeight = bounds.Height;
+					bounds.Height = bounds.Width;
+					bounds.Width = boundHeight;
+					transform = CGAffineTransform.MakeScale(-1.0f, 1.0f);
+					transform = CGAffineTransform.Rotate(transform, (float)Math.PI / 2.0f);
+					break;
+
+				case UIImageOrientation.Right:                                     //EXIF = 8
+					boundHeight = bounds.Height;
+					bounds.Height = bounds.Width;
+					bounds.Width = boundHeight;
+					transform = CGAffineTransform.MakeTranslation(imageSize.Height, 0.0f);
+					transform = CGAffineTransform.Rotate(transform, (float)Math.PI / 2.0f);
+					break;
+
+				default:
+					throw new Exception("Invalid image orientation");
+					break;
+			}
+
+			UIGraphics.BeginImageContext(bounds.Size);
+
+			CGContext context = UIGraphics.GetCurrentContext();
+
+			if (orient == UIImageOrientation.Right || orient == UIImageOrientation.Left)
+			{
+				context.ScaleCTM(-scaleRatio, scaleRatio);
+				context.TranslateCTM(-height, 0);
+			}
+			else
+			{
+				context.ScaleCTM(scaleRatio, -scaleRatio);
+				context.TranslateCTM(0, -height);
+			}
+
+			context.ConcatCTM(transform);
+			context.DrawImage(new RectangleF(0, 0, width, height), imgRef);
+
+			UIImage imageCopy = UIGraphics.GetImageFromCurrentImageContext();
+			UIGraphics.EndImageContext();
+
+			return imageCopy;
+		}
 
         private bool WriteFile(byte[] picture, string filePath)
         {
